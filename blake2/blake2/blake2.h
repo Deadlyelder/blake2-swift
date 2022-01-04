1ccb4ef4c95b133285f7fd138150ff9fe2b16cc0
Sankalp <mail@sghatpande.eu>
2022-01-04 09:09:48 +0100

    [Add] Initial structure

---
+++ blake2/blake2/blake2.h
@@ -0,0 +1,18 @@
+//
+//  blake2.h
+//  blake2
+//
+//  Created by Sankalp on 1/4/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for blake2.
+FOUNDATION_EXPORT double blake2VersionNumber;
+
+//! Project version string for blake2.
+FOUNDATION_EXPORT const unsigned char blake2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <blake2/PublicHeader.h>
+
+
