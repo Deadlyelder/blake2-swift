b99046665b50cf981686544bb7cfdde950ea5193
Sankalp <mail@sghatpande.eu>
2022-01-04 23:48:46 +0100

    [Add] tests

--- blake2/blake2/blake2utility.swift
+++ blake2/blake2/blake2utility.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import CommonCrypto
+
+extension String {
+    public func unhexlify() -> [UInt8] {
+        var pos = startIndex
+        return (0..<count/2).compactMap { _ in
+            defer { pos = index(pos, offsetBy: 2) }
+            return UInt8(self[pos...index(after: pos)], radix: 16)
+        }
+    }
+}
+
+extension Collection where Iterator.Element == UInt8 {
+    public func hexDescription() -> String {
+        return self.map({ String(format: "%02x", $0) }).joined()
+    }
+}
